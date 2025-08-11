5965e53120b197be6b043b31a763787d90d4444e
박여명 <qkrduaud123@gmail.com>
2025-08-11 20:06:18 +0900

    11720 숫자의 합 브론즈4 yeomyeoung

---
+++ yeomyeoung/BJ_11720.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BJ_11720 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        String str = scanner.next();
+        scanner.close();
+        int sum = 0;
+
+        String[] arr = str.split("");
+
+        for(int i = 0; i < length; i++){
+            sum += Integer.parseInt(arr[i]);
+        }
+
+        System.out.println(sum);
+    }
+}
