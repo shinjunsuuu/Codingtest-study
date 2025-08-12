9d2672bfcf0d2d7ebf4d2378b0e7d9238b270f30
박여명 <qkrduaud123@gmail.com>
2025-08-13 00:46:11 +0900

    2750 수 정렬하기 브론즈2 yeomyeoung

---
+++ yeomyeoung/BJ_2750.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BJ_2750 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int repeat = scanner.nextInt();
+        int[] arr = new int[repeat];
+        for (int i = 0; i < repeat; i++){
+            arr[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        for (int k = 1; k < repeat; k++){
+            for (int j = 0; j < (repeat - k); j ++){
+                int temp = 0;
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < repeat; i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
