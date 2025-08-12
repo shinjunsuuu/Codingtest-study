0949b94c00dba9e198bb8be24c4458e64f8ada49
shinjunsuuu <sjs438@gmail.com>
2025-08-12 12:14:37 +0900

    2750 / 수 정렬하기 / 브론즈2 / shinjunsuuu

---
+++ junsu/Solution_2750.java
@@ -0,0 +1,30 @@
+package junsu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Solution_2750 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		int arr[] = new int[x];
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0;i < x; i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < x; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
