eeade5d54f245f6e4fe340157e0d5290c7bb8c80
GIHYUN-LEE <gihyun506@naver.com>
2025-08-13 09:04:42 +0900

    2675/문자열반복/브론즈2, 2750/수 정렬하기/브론즈2, 2920/음계/브론즈2 LEE GIHYUN

---
+++ Gihyun/Solution4.java
@@ -0,0 +1,24 @@
+package Gihyun;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution4 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int N=scan.nextInt();
+		int []numbers= new int[N];
+		
+		for(int i=0;i<N;i++) {
+			int input=scan.nextInt();
+			numbers[i]=input;
+		}
+		Arrays.sort(numbers);
+		
+		for(int n=0;n<numbers.length;n++) {
+			System.out.println(numbers[n]);
+		}
+	}
+
+}
