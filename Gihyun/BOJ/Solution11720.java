33af800413efa35906d28bef6370ff41eb10aa7e
GIHYUN-LEE <gihyun506@naver.com>
2025-08-13 20:15:25 +0900

    구조변경

---
+++ Gihyun/BOJ/Solution11720.java
@@ -0,0 +1,30 @@
+package Gihyun.BOJ;
+
+import java.util.Scanner;
+
+public class Solution11720 {
+
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		int amount=scan.nextInt();
+		String number=scan.next();
+		scan.close();
+		int []arraynum=new int [amount];
+		
+		int index=0;
+		for(int i:number.toCharArray()) {
+			arraynum[index++]=i-'0';
+		}
+		
+		int sum=0;
+		for(int i:arraynum) {
+			sum+=i;
+		}
+		
+		System.out.println(sum);
+		// split 사용해보기
+		
+	
+	}
+
+}
---
+++ Gihyun/BOJ/Solution2675.java
@@ -0,0 +1,29 @@
+package Gihyun.BOJ;
+
+import java.util.Scanner;
+
+public class Solution2675 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int T=scan.nextInt();
+		
+		StringBuilder result=new StringBuilder();
+		
+		for(int i=0;i<T;i++) {
+			int R=scan.nextInt();
+			String S=scan.next();
+			
+			char[] arrayS=S.toCharArray();
+			
+			for(char m:arrayS) {
+				for(int r=0;r<R;r++) {
+					result.append(m);
+				}
+			}
+			result.append("\n");
+		}
+		scan.close();
+		System.out.print(result);
+	}
+}
