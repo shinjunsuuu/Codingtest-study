7cf3e296c739c4f7a9c33d69c20609bc9a2314ba
godnowoon <nowoon0426@naver.com>
2025-08-14 19:09:36 +0900

    5585 / 거스름돈 / 브론즈2 / 노운

---
+++ nowoon/BOJ/BOJ_5585.java
@@ -0,0 +1,27 @@
+package nowoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_5585 {
+//500 100 50 10 5 1
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+
+		int money = 1000 - Integer.parseInt(input);
+		int count = 0;
+		count += money / 500;
+		count += (money % 500) / 100;
+		count += (money % 100) / 50;
+		count += (money % 50) / 10;
+		count += (money % 10) / 5;
+		count += (money % 5) / 1;
+		System.out.println(count);
+
+	}
+
+}
