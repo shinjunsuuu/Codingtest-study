29c189f46052beb0632f2877deea50d73e3674a4
junsu <junsu000921@gmail.com>
2025-08-15 19:56:31 +0900

    refactor file names / BJ -> BOJ

--- junsu/BOJ/BOJ_2920.java
+++ junsu/BOJ/BOJ_2920.java
@@ -1,10 +1,10 @@
-package junsu;
+package junsu.BOJ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solution_2920 {
+public class BOJ_2920 {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
