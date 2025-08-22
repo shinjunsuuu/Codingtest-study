28c435ee8e65dc61db6ff64e250986528c11adb6
shinjunsuuu <sjs438@gmail.com>
2025-08-22 12:29:27 +0900

    1316 / 그룹 단어 체커 / 실버5 / shinjunsuuu

---
+++ junsu/BOJ/BOJ_1316.java
@@ -0,0 +1,31 @@
+package junsu.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1316 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int result = n;
+		for(int i = 0; i < n; i++) {
+			String s = br.readLine();
+			
+			for(int j = 0; j<s.length()-1;j++) {
+				if(s.charAt(j) == s.charAt(j+1)) {
+					continue;
+				}
+				else if(s.substring(j + 1).indexOf(s.charAt(j)) != -1) {
+					result -= 1;
+					break;
+				}
+			}
+			
+		}
+		System.out.println(result);
+	}
+
+}
